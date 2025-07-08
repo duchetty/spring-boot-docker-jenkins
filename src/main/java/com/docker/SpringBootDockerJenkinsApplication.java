73cc2cece477b084a593e86dbfd84fcaab6f26c8
duchetty <duchettydevendra@gmail.com>
2025-07-08 10:47:47 +0530

    intial commit

---
+++ src/main/java/com/docker/SpringBootDockerJenkinsApplication.java
@@ -0,0 +1,24 @@
+package com.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/welocme")
+public class SpringBootDockerJenkinsApplication {
+	
+	@GetMapping("/msg")
+	public String getMessage()
+	{
+		return "Spring boot Application with docker and jenkins";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerJenkinsApplication.class, args);
+	}
+
+}
